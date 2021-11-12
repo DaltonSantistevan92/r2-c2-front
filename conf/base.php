69268af47e398b1d8ed05cf707a511d356f0a3ce
Sergio Roger <sergio.lkg.31@gmail.com>
2021-11-11 22:39:51 -0500

    Nueva version subida

--- conf/base.php
+++ conf/base.php
@@ -1,6 +1,9 @@
 <?php
 
-define('BASE', 'http://192.168.100.107/escuela_2.0/r2-c2-front/');
-define('SERVIDOR', 'http://192.168.100.213/escuela_2.0/r2-c2-back/');
+// define('BASE', 'http://192.168.100.107/escuela_2.0/r2-c2-front/');
+// define('SERVIDOR', 'http://192.168.100.213/escuela_2.0/r2-c2-back/');
+define('BASE', 'http://localhost/r2-c2-front/');
+define('SERVIDOR', 'http://localhost/r2-c2-back/');
+
 define('APLICACION' , 'Nuevos Horizontes');
 date_default_timezone_set("America/Guayaquil");
