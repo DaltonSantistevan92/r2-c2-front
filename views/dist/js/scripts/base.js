1f50fa590af927013421ba516fb5ebf6e91f4e23
juan galarza <juanca_20201@outlook.es>
2021-11-17 22:37:43 -0500

    modulo de ticket e impresion

--- views/dist/js/scripts/base.js
+++ views/dist/js/scripts/base.js
@@ -1,5 +1,2 @@
-// const urlServidor = "http://192.168.100.213/escuela_2.0/r2-c2-back/";
-// const urlCliente = "http://192.168.100.107/escuela_2.0/r2-c2-front/";
-
-const urlServidor = "http://localhost/r2-c2-back/";
-const urlCliente = "http://localhost/r2-c2-front/";
+const urlServidor = "http://192.168.100.213/escuela_2.0/r2-c2-back/";
+const urlCliente = "http://192.168.100.108/escuela_2.0/r2-c2-front/";
