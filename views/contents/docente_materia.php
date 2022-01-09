9e22b2067628eddcc77dad6c5cbd9cb5cba3eb80
Sergio Roger <sergio.lkg.31@gmail.com>
2022-01-09 13:02:41 -0500

    Carga y seleccion de materia ene asignacion

--- views/contents/docente_materia.php
+++ views/contents/docente_materia.php
@@ -36,9 +36,9 @@
                             </div>
 
                             <div class="col-12 d-flex mb-3">
-                                <input type="hidden">
-                                <input type="text" class="form-control" placeholder="Materia" readonly>
-                                <button class="btn btn-sm btn-primary">
+                                <input type="hidden" id="dm-materia-id">
+                                <input type="text" class="form-control" placeholder="Materia" readonly id="dm-materia-texto">
+                                <button class="btn btn-sm btn-primary" id="btn-modal-materia">
                                     <i class="fas fa-search"></i>
                                 </button>
                             </div>
@@ -46,7 +46,7 @@
                             <div class="col-12 d-flex mb-3">
                                 <input type="hidden">
                                 <input type="text" class="form-control" placeholder="Docente" readonly>
-                                <button class="btn btn-sm btn-primary">
+                                <button class="btn btn-sm btn-primary" id="btn-modal-docente">
                                     <i class="fas fa-search"></i>
                                 </button>
                             </div>
@@ -78,18 +78,25 @@
             <div class="col-12 col-md-8 col-lg-9">
                 <div class="card">
                     <div class="card-header">
-                        <h5 class="m-0">Nueva Materia</h5>
+                        <h5 class="m-0">Nueva Asignación</h5>
                     </div>
 
                     <div class="card-body">
                         <div class="row mb-3">
                             <div class="col-12 col-md-5 col-lg-4">
+                                <label for="">Periodo</label>
+                                <select id="select-periodo-visualizar" class="form-control">
+                                    <option value="0">Seleccione una opción</option>
+                                </select>
+                            </div>
+
+                            <div class="col-12 col-md-5 col-lg-3">
                                 <label for="">Grado</label>
                                 <select id="select-grado-lista" class="form-control">
                                     <option value="0">Seleccione una opción</option>
                                 </select>
                             </div>
-                            <div class="col-12 col-md-5 col-lg-4">
+                            <div class="col-12 col-md-5 col-lg-3">
                                 <label for="">Paralelo</label>
                                 <select id="select-paralelo-lista" class="form-control">
                                     <option value="0">Seleccione una opción</option>
@@ -97,7 +104,7 @@
                             </div>
 
                             <div class="col-12 col-md-2 col-lg-2">
-                                <button class="btn btn-sm btn-primary" style="margin-top: 30px;">
+                                <button class="btn btn-sm btn-primary" style="margin-top: 33px;">
                                     <i class="fas fa-check"></i>
                                 </button>
                             </div>
@@ -108,29 +115,25 @@
                                 <thead class="bg-primary">
                                     <tr>
                                         <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col">Last</th>
-                                        <th scope="col">Handle</th>
+                                        <th scope="col">Docente</th>
+                                        <th scope="col">Materia</th>
+                                        <th scope="col">Grado</th>
+                                        <th scope="col">Paralelo</th>
+                                        <th scope="col">Acciones</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
                                         <th scope="row">1</th>
                                         <td>Mark</td>
-                                        <td>Otto</td>
                                         <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
+                                        <td>@mdo</td>
+                                        <td>@mdo</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-danger">
+                                                X
+                                            </button>
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -141,6 +144,128 @@
         </div>
     </div>
 </div>
+</div>
+
+<!-- Modal Materia-->
+<div class="modal fade" id="modalMateria" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="modalMateriaLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalMateriaLabel">Materias disponibles</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">Areas</label>
+                                <select id="modal-select-area" class="form-control">
+                                    <option value="0">Todos</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <div>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Area</th>
+                                            <th scope="col">Materia</th>
+                                            <th scope="col">Duración</th>
+                                            <th scope="col"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="table-modal-materia">
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                            <td>
+                                                <button class="btn btn-sm btn-outline-dark" onclick="selectMateria(1)">
+                                                    <i class="fas fa-check"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal docente -->
+<div class="modal fade" id="modalDocente" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="modalMateriaLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalMateriaLabel">Materias disponibles</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">Buscar docente</label>
+                                <input type="text" class="form-control" id="txt-buscar-docente">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <div>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Cedula</th>
+                                            <th scope="col">Nombres</th>
+                                            <th scope="col">Apellidos</th>
+                                            <th scope="col"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="table-modal-docente">
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                            <td>
+                                                <button class="btn btn-sm btn-outline-dark" onclick="selectDocente(1)">
+                                                    <i class="fas fa-check"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script src="<?= BASE ?>views/dist/js/scripts/docente_materia.js"></script>
--- views/dist/js/scripts/docente_materia.js
+++ views/dist/js/scripts/docente_materia.js
@@ -1,4 +1,6 @@
 
+const AREA_ID = 0;
+
 _init();
 
 function _init(){
@@ -6,6 +8,11 @@ function _init(){
     getGrados();
     getParalelos();
     asignar();
+    openModalMateria();
+    getAreas();
+    loadTableMaterias(AREA_ID);
+    changeSelectArea();
+    openModalDocente();
 }
 
 function getPeridos() {
@@ -16,6 +23,7 @@ function getPeridos() {
     .then(data => {
         if (data.data.length > 0) {
             const select = document.getElementById('select-periodo');
+            const select2 = document.getElementById('select-periodo-visualizar');
             
             let option = `<option value="0">Seleccione una opción</option>`;
 
@@ -24,6 +32,7 @@ function getPeridos() {
             });
 
             select.innerHTML = option;
+            select2.innerHTML = option;
         }
     });
 }
@@ -78,4 +87,119 @@ function asignar(){
     btn_asingar.addEventListener('click', () => {
         console.log("Recoger la data y armar json");
     });
+}
+
+function getAreas(){
+    const url = urlServidor + 'area/listar';
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        if (data.status) {
+            const select = document.getElementById('modal-select-area');
+            
+            let option = '<option value="0">Todos</option>';
+            
+            data.area.forEach(element => {
+                option += `<option value="${element.id}">${element.detalle}</option>`;
+            });
+
+            select.innerHTML = option;
+        }
+    });
+}
+
+ function openModalMateria(){
+     const btn = document.getElementById('btn-modal-materia');
+
+     btn.addEventListener('click', () => {
+         $('#modalMateria').modal('show');
+     })
+ }
+
+function openModalDocente(){
+    const btn = document.getElementById('btn-modal-docente');
+
+     btn.addEventListener('click', () => {
+         $('#modalDocente').modal('show');
+         const input = document.getElementById('txt-buscar-docente');
+         input.value = '';
+     })
+}
+
+function changeSelectArea(){
+    const select = document.getElementById('modal-select-area');
+
+    select.addEventListener('change', (event) => {
+        const id = event.target.value;
+        loadTableMaterias(id);
+    });
+}
+
+function loadTableMaterias(area_id){
+    const url = urlServidor + 'materias/listar/' + area_id;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const tbody = document.getElementById('table-modal-materia');
+        tbody.innerHTML = '';
+
+        if (data.status) {
+            let tr = '';
+
+            if(data.materia.length > 0){
+                data.materia.forEach((element, index) => {
+                    tr += `
+                    <tr>
+                        <th scope="row">${index+ 1}</th>
+                        <td>${element.area.detalle}</td>
+                        <td>${element.materia}</td>
+                        <td>${element.duracion}</td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-dark" onclick="selectMateria(${element.id})">
+                                <i class="fas fa-check"></i>
+                            </button>
+                        </td>
+                    </tr>`;
+                });
+            }else{
+                toastr.options = {
+                    "closeButton": true,
+                    "preventDuplicates": true,
+                    "positionClass": "toast-top-center",
+                };
+    
+                toastr["error"]("No hay datos", "Información")
+            }
+
+            tbody.innerHTML = tr;
+        }
+    });
+}
+
+function selectMateria(materia_id){
+    const url =  urlServidor + 'materias/listarId/' + materia_id;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+       
+        if (data.status) {
+            console.log(data);   
+
+            input_materia_id = document.getElementById('dm-materia-id');
+            input_materia_texto = document.getElementById('dm-materia-texto'); 
+
+            input_materia_id.value = materia_id;
+            input_materia_texto.value = data.materia.materia;
+        }
+    });
+
+    $('#modalMateria').modal('hide');
+}
+
+function selectDocente(docente_id){
+    console.log(docente_id);
+    $('#modalDocente').modal('hide');
 }
